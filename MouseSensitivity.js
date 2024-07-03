c4c893dfd016fca70e8523cb6e1e347164def353
Hamza Aydın <95700422+183461@users.noreply.github.com>
2024-07-03 17:39:00 +0300

    Update MouseSensitivity.js

--- MouseSensitivity.js
+++ MouseSensitivity.js
@@ -2,14 +2,27 @@ const fare = {
   renk: "siyah",
   hiz: 5,
   hizDegistir: function(degisimYonu) {
-    if (degisimYonu === "arttır" && this.hiz < 10) {
-      this.hiz = this.hiz + 1;
-      return "Yeni hız " + this.hiz;
-    } else if (degisimYonu === "azalt" && this.hiz > 1) {
-      this.hiz = this.hiz - 1;
-      return "Yeni hız " + this.hiz
-    } else {
+    if (degisimYonu === "arttır") 
+    {
+      if (this.hiz < 10) {
+        this.hiz = this.hiz + 1;
+      }
+      return "yeni hız: " + this.hiz;
+    } 
+    else if (degisimYonu === "azalt") 
+    {
+      if (this.hiz > 1) {
+        this.hiz = this.hiz - 1;
+      }
+      
+      return "yeni hız: " + this.hiz;
+    } 
+    else {
       return "yanlış argüman"
     }
-  } 
+  }
 }
+
+console.log(fare.hizDegistir("azalt"));
+console.log(fare.hizDegistir("azalt"));
+console.log(fare.hizDegistir("azalt"));
